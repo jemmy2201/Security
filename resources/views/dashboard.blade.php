b9d36ed51e46f996834c11b659dcb217489d7b4e
jemmy <jemmyrolish07@gmail.com>
2021-03-22 15:08:13 +0700

    fix ui login (desktop & phone)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,100 +1,66 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app_welcome')
 
-        <title>Laravel</title>
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 hidden-xs" style="margin-top: 150px;margin-left: 150px;">
+                        <h1><b>Welcome to Union of </b></h1>
+                        <h1><b>Security Employees</b></h1>
+                    </div>
+                    <div class="col-sm-6 visible-xs hidden-md">
+                        <h1><b>Welcome to Union of </b></h1>
+                        <h1><b>Security Employees</b></h1>
+                    </div>
+                    <div class="col-sm-4" style="border-style: groove; background: white">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a data-toggle="tab" href="#scan">Singpass app</a></li>
+                            <li><a data-toggle="tab" href="#passlogin">Password login</a></li>
+                        </ul>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+                        <div class="tab-content">
+                            <div id="scan" class="tab-pane fade in active">
+                                <center>
+                                    <h3><b>Scan with Singpass app</b></h3>
+                                <h4>to log in</h4>
+                                </center>
+                                <img src="img/barcode_singpass.png">
+                                <center>
+                                <p>Don't have Singapass app?<a href="https://app.singpass.gov.sg/" target="_blank">Download now</a></p>
+                                </center>
+                            </div>
+                            <div id="passlogin" class="tab-pane fade">
+                                <h3>Log in</h3>
+                                <form>
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" name="singpass_id" id="singpass_id" aria-describedby="emailHelp" placeholder="SingPass ID">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                                    </div>
+                                    <button type="submit" class=" btn btn-danger btn-lg btn-block ">Log in</button>
+                                    <br>
+                                    <div class="row">
+                                        <div class="col">
+                                            <a href="https://www.singpass.gov.sg/singpass/retrieveaccount/retrievesingpassid" target="_blank">Forgot Singpass ID</a>
+                                        </div>
+                                        <div class="col" style="margin-left: 74px;">
+                                            <a href="https://www.singpass.gov.sg/singpass/onlineresetpassword/userdetail"target="_blank">Reset Password</a>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <center>
+                                    <h4 style="border-style: groove;padding: 10px"><a style="color: #808080;text-decoration: none;" href="https://www.singpass.gov.sg/singpass/register/instructions" target="_blank">Register for Singpass</a></h4>
+                                    </center>
+                                    <br>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
