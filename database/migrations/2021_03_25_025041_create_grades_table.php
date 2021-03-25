2587c8c7b1546cb270fe6c2a9fcc12c615fd24f0
jemmy <jemmyrolish07@gmail.com>
2021-03-25 10:31:25 +0700

    fix revisi db 2

---
+++ database/migrations/2021_03_25_025041_create_grades_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->string('application_id')->nullable()->comment('get table application');
+            $table->string('name');
+            $table->string('bsoc');
+            $table->string('ssoc');
+            $table->string('sssc');
+            $table->string('created_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grades');
+    }
+}
