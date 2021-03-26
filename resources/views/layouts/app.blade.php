994520ae57a0db1f9cc1466199455c306706992a
jemmy <jemmyrolish07@gmail.com>
2021-03-26 19:13:29 +0700

    fix konek backend & frontend step 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.js"></script>
     {{--End Popup Alert--}}
+
 </head>
 <body >
     <meta name="csrf-token" content="{{ csrf_token() }}" />
