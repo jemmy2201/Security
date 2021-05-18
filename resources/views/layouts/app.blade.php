62aabd2975f898a8ab4979748ba67f08c130bf2b
jemmy <jemmyrolish07@gmail.com>
2021-05-18 09:16:31 +0700

    test doank 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-{{--   test --}}
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
