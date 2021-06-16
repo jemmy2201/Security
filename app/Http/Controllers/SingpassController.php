8872e5568f6d6bb337ba94c41ee76125aef3a982
jemmy <jemmyrolish07@gmail.com>
2021-06-16 22:32:11 +0700

    singpass step 2

--- app/Http/Controllers/SingpassController.php
+++ app/Http/Controllers/SingpassController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+
+use Auth;
 
 class SingpassController extends Controller
 {
     public function login()
     {
-
+        $existingUser = User::where('passid',"S9812381D")->first();
+        if($existingUser){
+            auth()->login($existingUser, true);
+            return redirect()->to('/home');
+        }
+        return redirect()->to('/');
     }
 }
