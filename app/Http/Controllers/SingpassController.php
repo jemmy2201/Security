30cd4e78b77db0e8d78bc5657ac9b700e59f3a7d
jemmy <jemmyrolish07@gmail.com>
2021-07-04 14:17:15 +0700

    revisi jwks

--- app/Http/Controllers/SingpassController.php
+++ app/Http/Controllers/SingpassController.php
@@ -19,14 +19,25 @@ class SingpassController extends Controller
         return redirect()->to('/');
     }
     public function jwks(){
-        $key =array(
-                "kty"=> "RSA",
-                "e"=> "AQAB",
-                "use"=> "enc",
-                "kid"=> "CgRfvss8Qw7BuTyLxSPyueurzMc=",
-                "alg"=> "RSA-OAEP",
-                "n"=> "lPmNltQXrer-6nzHs0CvGGdufw5AC-33tlbzfFjvSlulSSHQSeegU-KSqNkfnpAgMOMOD01MCpqg_SefhJ8pTapYIJdj1X8Df7t1GUzxD1TEHhQpIy0HvdYQgjl2TseNVirG9Vr4I78X_tG4hIyOewLKKqYGqBXDg41pg40QYPqsvAYJ8vZvg7lvyMaKPySnzVZFWEUtGrCXjfpkuImH_UmjdJU31drS1-YjfjMB6LZG7UMoY99nyf3y-_IHqraho5j2-3r5IZcg6nUznNp2NybGB1AG0YWCFqb-0BfBuaa9kuBVjjGe3iQSrRsDHGWA-8ZgOE7KhjTakQw_UPVXlw"
-            );
+        $key['keys'] =[array(
+            "kty"=> "EC",
+            "d"=> "AGscyf2X8C0VREczpb3E_yUVHNnr5DeuDD-YQqgRBsn9d9GML6iELO8OayDWas7kSMABFpxT_Nk0AK0OTMo1s7zp",
+            "use"=> "sig",
+            "crv"=>"P-521",
+            "kid"=> "idx",
+            "x"=> "AQUrwZ5XP8Dk_Ivj4u9ZJ7wPkIiTykeyy2VzkB39izR8is5jXnPBLbZUpSn30Y92U_XT8j-u-9lsPpPlUBhM2z6H",
+            "y"=> "AFK8aR2UDfnhhTZcgcoB6-EGKzR_AWwMDOlljzzWDwkWrMsVs7WkUGTDFjkUMT3sZqm36k2s-Ppw_T4DAhiQ_wsg",
+            "alg"=> "ES512"
+            ),array(
+            "kty"=> "EC",
+            "d"=> "Af813aqlv539RquwDsaA86z9uHk1FvictvCc-NirMAeeT5pGq7Z121ErAjcOHlZCVOs-keiYcAumcG-dSulvwAJ0",
+            "use"=> "enc",
+            "crv"=> "P-521",
+            "kid"=> "idx2",
+            "x"=> "AfiIONfcS3x-klWcRYbsH9TmXvAiQOK6kOKEDyi6rYRW3ZQJCTX-feJu9I8lvB03bsfuNhk5EHjXqe4qP-BBS3Ae",
+            "y"=> "AZS5Ngw4suPgKw6VvJvEz2-SmAbkJk-8iXycFcBLK0kdxIfhWZz3OBHDVBff1NZGamwjReZIeB0tAT9BG7J94H1e",
+            "alg"=> "ECDH-ES+A256KW"
+        )];
         return $key;
     }
     public function dummy_login($type)
