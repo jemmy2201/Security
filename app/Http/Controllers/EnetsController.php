cc872a9ae223713dc3338644e243d2799bee6f15
jemmy <jemmyrolish07@gmail.com>
2021-08-27 19:31:59 +0700

    enets Controller

---
+++ app/Http/Controllers/EnetsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EnetsController extends Controller
+{
+    public function s2sTxnEndURL()
+    {
+        return 'success';
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function b2sTxnEndURL()
+    {
+        return 'success';
+    }
+}
