0047ed045e25ce72545c43a7d14d1c536ae70244
jemmy <jemmyrolish07@gmail.com>
2021-08-28 11:36:17 +0700

    revisi singpass step 17

--- app/Http/Controllers/SingpassController.php
+++ app/Http/Controllers/SingpassController.php
@@ -137,6 +137,7 @@ class SingpassController extends Controller
                 'redirect_uri' => redirectUrlSingpass,
                 'code' => $code,
             ];
+            $data = 'client_assertion_type = urn%3Aietf%3Aparams%3Aoauth%3Aclient-assertion-type%3Ajwt-bearer&client_assertion = '.$jwt.'&client_id = '.clientIdSinpass.'&grant_type = authorization_code&redirect_uri = '.redirectUrlSingpass.'&code = '.$code.'';
 //
 //        $client = new Client();
 //
