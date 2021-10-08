dee9c4b5dc8968318dad4568b637e3e0cf8108b6
jemmy <jemmyrolish07@gmail.com>
2021-10-08 15:18:52 +0700

    revisi 5 enets

--- app/Http/Controllers/EnetsController.php
+++ app/Http/Controllers/EnetsController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\booking_schedule;
+use App\t_grade;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 class EnetsController extends Controller
 {
@@ -11,7 +16,7 @@ class EnetsController extends Controller
         $this->middleware('auth');
     }
 
-    public function frontend_response(Request $request)
+    public function s2sTxnEndURL(Request $request)
     {
         die(print_r($request->all()));
     }
@@ -21,8 +26,28 @@ class EnetsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function backend_response(Request $request)
+    public function b2sTxnEndURL(Request $request)
     {
-        die(print_r($request->all()));
+        $BookingScheduleAppointment = booking_schedule::where(['nric' => Auth::user()->nric,'card_id'=>$request->session()->all()['card']])
+            ->update([
+                'gst_id' => $request->session()->all()['grade_id'],
+                'trans_date' => date('d/m/Y H:i:s'),
+//                'expired_date' => date('Y-m-d', strtotime('+1 years')),
+                'paymentby' => "Enets",
+                'status_payment' => paid,
+                'grand_total' => $request->session()->all()['grand_total'],
+//                'receiptNo' => $this->receiptNo(),
+                'status_app' => submitted,
+                'transaction_amount_id' => $request->session()->all()['transaction_amount'],
+            ]);
+
+        $request->merge(['app_type' => $request->session()->all()['app_type'], 'thank_payment' => true,'card' => $request->session()->all()['card'],'router_name' => Route::getCurrentRoute()->getActionName()]);
+        $course = User::leftjoin('booking_schedules', 'users.nric', '=', 'booking_schedules.nric')
+            ->where(['booking_schedules.nric' => Auth::user()->nric,'booking_schedules.card_id'=>$request->session()->all()['card']])->first();
+        $t_grade = t_grade::get();
+
+        return view('view_courses')->with(['t_grade' => $t_grade,'courses' => $course, "request" => $request]);
+
     }
+
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1006,29 +1006,6 @@ class HomeController extends Controller
         return $clear_data;
     }
 
-    public function b2sTxnEndURL(Request $request)
-    {
-        $BookingScheduleAppointment = booking_schedule::where(['nric' => Auth::user()->nric,'card_id'=>$request->session()->all()['card']])
-            ->update([
-                'gst_id' => $request->session()->all()['grade_id'],
-                'trans_date' => date('d/m/Y H:i:s'),
-//                'expired_date' => date('Y-m-d', strtotime('+1 years')),
-                'paymentby' => "Enets",
-                'status_payment' => paid,
-                'grand_total' => $request->session()->all()['grand_total'],
-//                'receiptNo' => $this->receiptNo(),
-                'status_app' => submitted,
-                'transaction_amount_id' => $request->session()->all()['transaction_amount'],
-            ]);
-
-        $request->merge(['app_type' => $request->session()->all()['app_type'], 'thank_payment' => true,'card' => $request->session()->all()['card'],'router_name' => Route::getCurrentRoute()->getActionName()]);
-        $course = User::leftjoin('booking_schedules', 'users.nric', '=', 'booking_schedules.nric')
-            ->where(['booking_schedules.nric' => Auth::user()->nric,'booking_schedules.card_id'=>$request->session()->all()['card']])->first();
-        $t_grade = t_grade::get();
-
-        return view('view_courses')->with(['t_grade' => $t_grade,'courses' => $course, "request" => $request]);
-
-    }
     protected  function  NewPayment($request){
         if ($request['payment_method'] == paynow){
             $payment_method = 'paynow';
