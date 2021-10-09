40fccc9c801d9be0bd2d49759dcf8066be1e0074
jemmy <jemmyrolish07@gmail.com>
2021-10-09 09:41:11 +0700

    revisi 13 enets get response from enets

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/qrcode', function () {
 })->name('qrcode');
 
 // Enets
-Route::put('/s2sTxnEndURL', 'EnetsController@s2sTxnEndURL');
+Route::post('/s2sTxnEndURL', 'EnetsController@s2sTxnEndURL');
 Route::get('/b2sTxnEndURL', 'EnetsController@b2sTxnEndURL');
 
 //Route::post('/enets/frontend_response', 'EnetsController@frontend_response');
