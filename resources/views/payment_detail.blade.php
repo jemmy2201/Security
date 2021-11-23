8349bcd4bc47e8b0198bb9abcfd2b95b456f7dd4
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2021-11-23 16:47:13 +0700

    add time loading payment pages

--- resources/views/payment_detail.blade.php
+++ resources/views/payment_detail.blade.php
@@ -440,7 +440,7 @@
     $(window).ready(hideLoader);
 
     // Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
-    setTimeout(hideLoader, 20 * 1000);
+    setTimeout(hideLoader, 2000000 * 1000);
 
     $( document ).ready(function() {
         //Create a PaynowQR object
