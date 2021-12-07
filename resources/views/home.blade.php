36d75179af449ef74a4129e04f3119870a41d430
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2021-12-07 12:46:20 +0700

    change flow bisnis login revisi 4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,21 +6,57 @@
 </style
 @section('content')
     <div class="container">
-        <iframe src ="{{ asset('/USE ID Card Portal Terms Of Use.pdf') }}" width="100%" height="100%"></iframe>
+        <h4 style="font-weight: bold;">
+            To continue further, you must agree to the terms of user for accessing this web portal. Read <a href="#"> <span style="color: blue;" id="open_terms">TERMS OF USE </span></a>document.
+        </h4>
+    </div>
+    <div class="container">
+        <iframe src ="{{ asset('/USE ID Card Portal Terms Of Use.pdf') }}" width="100%" height="100%" style="display: none;" id="view_terms"></iframe>
     </div>
     <br>
     <div class="container">
         <div class="row">
             <div class="col-6">
-                <input class="form-check-input" type="checkbox" id="agree_pdf" width="120%">
-                <span style="margin-left: 20px;font-weight: bold;font-size: 15px;">Agree</span>
+                <h4>
+                <input class="form-check-input" name="agree_pdf" type="checkbox" id="agree_pdf" width="120%">
+                <span style="margin-left: 20px;font-weight: bold;">Accept Terms Of Use and proceed</span>
+                </h4>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <button class=" btn btn-light btn-lg btn-block" style="border-style: groove; background: black; color: #E31D1A" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"  >
+                    <a href="" style="text-decoration:none; color: white;">
+                        Back
+                    </a>
+                </button>
+            </div>
+            <div class="col-2">
+                <button class=" btn btn-light btn-lg btn-block" style="border-style: groove; background: black; color: #E31D1A" id="next">
+                    <a href="" style="text-decoration:none; color: white;">
+                        Next
+                    </a>
+                </button>
             </div>
         </div>
     </div>
 <script type="application/javascript">
     $( document ).ready(function() {
-        $('#agree_pdf').on('click', function () {
-            window.location.href = "/landing_page";
+        // $('#agree_pdf').on('click', function () {
+        //     window.location.href = "/landing_page";
+        // });
+        $('#open_terms').on('click', function () {
+            $("#view_terms").css("display", "block");
+        });
+
+        $('#next').on('click', function () {
+            if ($("input[name='agree_pdf']:checked").val()) {
+                    window.location.href = "/landing_page";
+            }else{
+                swal("Error!", "Tick Accept the Terms of Use and continue.", "error")
+            }
         });
     });
 </script>
