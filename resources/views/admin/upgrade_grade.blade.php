5428f86f59b1cc8af86808e35a1f87d62cafe668
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2022-01-03 00:15:21 +0700

    set timeout ajax

--- resources/views/admin/upgrade_grade.blade.php
+++ resources/views/admin/upgrade_grade.blade.php
@@ -357,6 +357,8 @@
             // proses upload via AJAX disubmit ke 'upload.php'
             // selama proses upload, akan menjalankan progressHandler()
             var ajax = new XMLHttpRequest();
+            ajax.timeout = 200000000; // time in milliseconds
+
             ajax.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
                     document.getElementById("import_completed").innerHTML = "Import Records Completed";
