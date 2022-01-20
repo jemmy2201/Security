3abc2c275531f2a6073fc10d3b26de0d37bef3be
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2022-01-21 06:56:02 +0700

    revisi 1 update so

---
+++ app/so_update_info.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class so_update_info extends Model
+{
+    protected $table = 'so_update_info';
+
+    protected $fillable = [
+        'PassID', 'NRIC', 'Name','Grade','New_Grade','TR_RTT','TR_CSSPB','TR_CCTC','TR_HCTA','TR_X_RAY','SKILL_BFM','SKILL_BSS','SKILL_FSM','SKILL_CERT','SKILL_COSEM','Date_Submitted'
+    ];
+}
