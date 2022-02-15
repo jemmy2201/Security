d787664076bddf5e63b58a27bbec5fcb5069ba4d
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2022-02-15 07:44:09 +0700

    revisi 1 soquery

--- resources/views/soquery.blade.php
+++ resources/views/soquery.blade.php
@@ -12,12 +12,12 @@
             <div class="col-4"style="text-align: left"><h2><b>NAME<span style="margin-left: 165px;">:</span></b></h2></div>
             <div class="col-6" style="color: white;"><h2>{{$soquery->Name}}</h2></div>
             <div class="w-100"></div>
-            @php
-                $cutnric = substr($soquery->Nric, -4);
-                $nric = "XXXXX$cutnric";
-            @endphp
-            <div class="col-4"style="text-align: left"><h2><b>NRIC/FIN<span style="margin-left: 125px;">:</span></b></h2></div>
-            <div class="col-6" style="color: white;"><h2>{{$nric}}</h2></div>
+{{--            @php--}}
+{{--                $cutnric = substr($soquery->Nric, -4);--}}
+{{--                $nric = "XXXXX$cutnric";--}}
+{{--            @endphp--}}
+{{--            <div class="col-4"style="text-align: left"><h2><b>NRIC/FIN<span style="margin-left: 125px;">:</span></b></h2></div>--}}
+{{--            <div class="col-6" style="color: white;"><h2>{{$nric}}</h2></div>--}}
             <div class="w-100"></div>
             <div class="col-4"style="text-align: left"><h2><b>PASS ID<span style="margin-left: 137px;">:</span></b></h2></div>
             <div class="col-6" style="color: white;"><h2>{{$soquery->PassID}}</h2></div>
@@ -33,7 +33,7 @@
     <div class="col-8">
         <h1><b>TRAINING COMPETENCY</b></h1>
         <div class="row">
-            @if(substr( $soquery->Course, -5,1) == true)
+            @if($soquery->TR_CCTC == "YES")
             <div class="col-4"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_1.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
             <div class="col-8" style="color: white;margin-left: -145px;margin-top: 32px;">
                 <h2>CCTC<br>
@@ -41,7 +41,7 @@
             </div>
             <div class="w-100"></div><br>
             @endif
-            @if(substr( $soquery->Course, -4,1) == true)
+            @if($soquery->TR_CSSPB == "YES")
             <div class="col-4"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_2.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
             <div class="col-8" style="color: white;margin-left: -145px;margin-top: 32px;">
                 <h2>CSS-P/B <br>
@@ -50,7 +50,7 @@
             <div class="w-100"></div>
             @endif
 
-            @if(substr( $soquery->Course, -3,1) == true)
+            @if($soquery->TR_X_RAY== "YES")
             <div class="col-4"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_3.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
             <div class="col-8" style="color: white;margin-left: -145px;margin-top: 32px;">
                 <h2>CSS-X<br>
@@ -59,7 +59,7 @@
             <div class="w-100"></div>
             @endif
 
-            @if(substr( $soquery->Course, -2,1) == true)
+            @if($soquery->TR_HCTA == "YES")
             <div class="col-4"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_4.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
             <div class="col-8" style="color: white;margin-left: -145px;margin-top: 32px;">
                 <h2>HCTA<br>
@@ -67,11 +67,11 @@
             </div>
             <div class="w-100"></div><br>
             @endif
-            @if(substr( $soquery->Course, -1,1) == true)
+            @if($soquery->TR_RTT == "YES")
             <div class="col-4"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_5.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
             <div class="col-8" style="color: white;margin-left: -145px;margin-top: 32px;">
                 <h2>RTT<br>
-                    Handle Counter Terrorism Activities</h2>
+                    Recognise Terrorist Threats</h2>
             </div>
             @endif
         </div>
@@ -79,7 +79,7 @@
     <div class="col-12">
         <div class="row">
             <div class="col-4"style="text-align: left"><h3><b>Date Of Updated Courses<span style="margin-left: 20px;">:</span></b></h3></div>
-            <div class="col-4" style="color: white;margin-left: -123px;"><h3>{{date('d F Y ',strtotime($soquery->LicenseExpiryDate))}}</h3></div>
+            <div class="col-4" style="color: white;margin-left: -123px;"><h3>{{date('d F Y ',strtotime($soquery->CreationDate))}}</h3></div>
 
             <div class="w-100"></div><br>
         </div>
@@ -87,16 +87,26 @@
     <div class="col-8">
         <h1><b>SKILL SETS ACQUIRED</b></h1>
         <div class="row">
+            @if($soquery->SKILL_BSS == "YES")
             <div class="col-6"style="text-align: left;color: white;" ><h2><b>Basic Facilities Management</b></h2></div>
             <div class="w-100"></div>
+            @endif
+            @if($soquery->SKILL_FSM == "YES")
             <div class="col-10"style="text-align: left;color: white;"><h2><b>Fundamentals of Building Services & Safety</b></h2></div>
             <div class="w-100"></div>
+            @endif
+            @if($soquery->SKILL_FSM == "YES")
             <div class="col-6"style="text-align: left;color: white;"><h2><b>Fire Safety Management</b></h2></div>
             <div class="w-100"></div>
+            @endif
+            @if($soquery->SKILL_CERT == "YES")
             <div class="col-6"style="text-align: left;color: white;"><h2><b>CERT</b></h2></div>
             <div class="w-100"></div>
+            @endif
+            @if($soquery->SKILL_COSEM == "YES")
             <div class="col-6"style="text-align: left;color: white;"><h2><b>COSEM</b></h2></div>
             <div class="w-100"></div>
+            @endif
         </div>
     </div>
 </div>
@@ -107,13 +117,13 @@
             <div class="col-12"style="text-align: left"><h2><b>NAME<span style="margin-left: 165px;">:</span></b></h2></div>
             <div class="col-12" style="color: white;"><h2>{{$soquery->Name}}</h2></div>
             <div class="w-100"></div>
-            @php
-                $cutnric = substr($soquery->Nric, -4);
-                $nric = "XXXXX$cutnric";
-            @endphp
-            <div class="col-12"style="text-align: left"><h2><b>NRIC/FIN<span style="margin-left: 125px;">:</span></b></h2></div>
-            <div class="col-12" style="color: white;"><h2>{{$nric}}</h2></div>
-            <div class="w-100"></div>
+{{--            @php--}}
+{{--                $cutnric = substr($soquery->Nric, -4);--}}
+{{--                $nric = "XXXXX$cutnric";--}}
+{{--            @endphp--}}
+{{--            <div class="col-12"style="text-align: left"><h2><b>NRIC/FIN<span style="margin-left: 125px;">:</span></b></h2></div>--}}
+{{--            <div class="col-12" style="color: white;"><h2>{{$nric}}</h2></div>--}}
+{{--            <div class="w-100"></div>--}}
             <div class="col-12"style="text-align: left"><h2><b>PASS ID<span style="margin-left: 137px;">:</span></b></h2></div>
             <div class="col-12" style="color: white;"><h2>{{$soquery->PassID}}</h2></div>
             <div class="w-100"></div>
@@ -128,7 +138,7 @@
     <div class="col-12">
         <h1><b>TRAINING COMPETENCY</b></h1>
         <div class="row">
-            @if(substr( $soquery->Course, -5,1) == true)
+            @if($soquery->TR_CCTC == "YES")
                 <div class="col-12"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_1.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
                 <div class="col-12" style="color: white;">
                     <h2>CCTC<br>
@@ -136,7 +146,7 @@
                 </div>
                 <div class="w-100"></div><br>
             @endif
-            @if(substr( $soquery->Course, -4,1) == true)
+            @if($soquery->TR_CSSPB == "YES")
                 <div class="col-12"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_2.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
                 <div class="col-12" style="color: white;">
                     <h2>CSS-P/B <br>
@@ -145,7 +155,7 @@
                 <div class="w-100"></div>
             @endif
 
-            @if(substr( $soquery->Course, -3,1) == true)
+            @if($soquery->TR_X_RAY == "YES")
                 <div class="col-12"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_3.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
                 <div class="col-12" style="color: white;">
                     <h2>CSS-X<br>
@@ -154,7 +164,7 @@
                 <div class="w-100"></div>
             @endif
 
-            @if(substr( $soquery->Course, -2,1) == true)
+            @if($soquery->TR_HCTA == "YESY")
                 <div class="col-12"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_4.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
                 <div class="col-12" style="color: white;">
                     <h2>HCTA<br>
@@ -162,11 +172,11 @@
                 </div>
                 <div class="w-100"></div><br>
             @endif
-            @if(substr( $soquery->Course, -1,1) == true)
+            @if($soquery->TR_RTT == "YES")
                 <div class="col-12"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_5.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
                 <div class="col-12" style="color: white;">
                     <h2>RTT<br>
-                        Handle Counter Terrorism Activities</h2>
+                        Recognise Terrorist Threats</h2>
                 </div>
             @endif
         </div>
@@ -174,22 +184,32 @@
     <div class="col-12">
         <div class="row">
             <div class="col-12"style="text-align: left"><h3><b>Date Of Updated Courses<span style="margin-left: 20px;">:</span></b></h3></div>
-            <div class="col-12" style="color: white;"><h3>{{date('d F Y ',strtotime($soquery->LicenseExpiryDate))}}</h3></div>
+            <div class="col-12" style="color: white;"><h3>{{date('d F Y ',strtotime($soquery->CreationDate))}}</h3></div>
         </div>
     </div>
     <div class="col-12">
         <h1><b>SKILL SETS ACQUIRED</b></h1>
         <div class="row">
+            @if($soquery->SKILL_BSS = "YES")
             <div class="col-12"style="text-align: left;color: white;" ><h2><b>Basic Facilities Management</b></h2></div>
             <div class="w-100"></div>
+            @endif
+            @if($soquery->SKILL_FSM = "YES")
             <div class="col-12"style="text-align: left;color: white;"><h2><b>Fundamentals of Building Services & Safety</b></h2></div>
             <div class="w-100"></div>
+            @endif
+            @if($soquery->SKILL_FSM = "YES")
             <div class="col-12"style="text-align: left;color: white;"><h2><b>Fire Safety Management</b></h2></div>
             <div class="w-100"></div>
+            @endif
+            @if($soquery->SKILL_CERT = "YES")
             <div class="col-12"style="text-align: left;color: white;"><h2><b>CERT</b></h2></div>
             <div class="w-100"></div>
+            @endif
+            @if($soquery->SKILL_COSEM = "YES")
             <div class="col-12"style="text-align: left;color: white;"><h2><b>COSEM</b></h2></div>
             <div class="w-100"></div>
+            @endif
         </div>
     </div>
 </div>
