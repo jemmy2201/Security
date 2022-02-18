e4d2466c995878e3c28592ba2ae3b5da2cb19d2e
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2022-02-18 10:19:35 +0700

    handle step 1 so query

--- config/constant.php
+++ config/constant.php
@@ -304,6 +304,7 @@ define('default_email', "email");
 
 define('n_card_issue', "N");
 define('value_card_issue', "Kindly contact Union Of Security Employees for further assistance. Contact details as shown on the top of this page.");
+define('value_IDQuery', "Pass ID not found.");
 define('value_expired_card', "Your ID card's expiry date has expired. Renew your PLRD SO/PI licence @ Go Business Singapore website - https://www.gobusiness.gov.sg.");
 
 define('phone_general_office', "+65 6381 9150 (General Office)");
