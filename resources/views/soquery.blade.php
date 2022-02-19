28fc84813482bf9fd637867535fc759a85189ceb
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2022-02-19 15:27:55 +0700

    revisi 2 phone design soquery.blade.php

--- resources/views/soquery.blade.php
+++ resources/views/soquery.blade.php
@@ -8,9 +8,9 @@
 {{--Dekstop--}}
 <div class="hidden-xs">
     <div class="col-8">
-        <h1><b>SECURITY OFFICER'S INFO</b></h1>
+        <h1 style="color: white;"><b>SECURITY OFFICER'S INFO</b></h1>
         <div class="row">
-            <div class="col-4"style="text-align: left"><h2><b>NAME<span style="margin-left: 165px;">:</span></b></h2></div>
+            <div class="col-4"style="color: white; text-align: left"><h2><b>NAME<span style="margin-left: 165px;">:</span></b></h2></div>
             <div class="col-6" style="color: white;"><h2>{{$soquery->Name}}</h2></div>
             <div class="w-100"></div>
 {{--            @php--}}
@@ -20,19 +20,24 @@
 {{--            <div class="col-4"style="text-align: left"><h2><b>NRIC/FIN<span style="margin-left: 125px;">:</span></b></h2></div>--}}
 {{--            <div class="col-6" style="color: white;"><h2>{{$nric}}</h2></div>--}}
             <div class="w-100"></div>
-            <div class="col-4"style="text-align: left"><h2><b>PASS ID<span style="margin-left: 137px;">:</span></b></h2></div>
+            <div class="col-4"style="color: white; text-align: left"><h2><b>PASS ID<span style="margin-left: 137px;">:</span></b></h2></div>
             <div class="col-6" style="color: white;"><h2>{{$soquery->PassID}}</h2></div>
             <div class="w-100"></div>
-            <div class="col-4"style="text-align: left"><h2><b>PWM GRADE<span style="margin-left: 65px;">:</span></b></h2></div>
+            <div class="col-4"style="color: white; text-align: left"><h2><b>PWM GRADE<span style="margin-left: 65px;">:</span></b></h2></div>
             <div class="col-6" style="color: white;"><h2>{{$soquery->Grade}}</h2></div>
             <div class="w-100"></div>
-            <div class="col-4"style="text-align: left"><h2><b>LICENSE EXPIRY<span style="margin-left: 7px;">:</span></b></h2></div>
+            <div class="col-4"style="color: white;text-align: left"><h2><b>LICENSE EXPIRY<span style="margin-left: 7px;">:</span></b></h2></div>
             <div class="col-6" style="color: white;"><h2>{{date('m/d/Y ',strtotime($soquery->LicenseExpiryDate))}}</h2></div>
             <div class="w-100"></div>
         </div>
     </div>
     <div class="col-8">
+        <div class="row" style="background-color: white; display: flex;
+  justify-content: center; /* horizontal */
+  align-items: center; ">
         <h1><b>TRAINING COMPETENCY</b></h1>
+        </div>
+        <br>
         <div class="row">
             @if($soquery->TR_CCTC == "YES")
             <div class="col-4"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_1.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
@@ -77,41 +82,44 @@
             @endif
         </div>
     </div>
-    <div class="col-12">
-        <div class="row">
-            <div class="col-4"style="text-align: left"><h3><b>Date Of Updated Courses<span style="margin-left: 20px;">:</span></b></h3></div>
-            <div class="col-4" style="color: white;margin-left: -123px;"><h3>{{date('d F Y ',strtotime($soquery->CreationDate))}}</h3></div>
-
-            <div class="w-100"></div><br>
-        </div>
-    </div>
+    <br>
     <div class="col-8">
+        <div class="col-8">
+            <div class="row" style="background-color: white; display: flex;
+  justify-content: center; /* horizontal */
+  align-items: center; ">
         <h1><b>SKILL SETS ACQUIRED</b></h1>
+            </div>
         <div class="row">
             @if($soquery->SKILL_BFM == "YES")
-            <div class="col-6"style="text-align: left;color: white;" ><h2><b>Basic Facilities Management</b></h2></div>
+            <div class="col-12"style="text-align: left;color: white;" ><h2><b>Basic Facilities Management</b></h2></div>
             <div class="w-100"></div>
             @endif
             @if($soquery->SKILL_BSS == "YES")
-            <div class="col-10"style="text-align: left;color: white;"><h2><b>Fundamentals of Building Services & Safety</b></h2></div>
+            <div class="col-12"style="text-align: left;color: white;"><h2><b>Fundamentals of Building Services & Safety</b></h2></div>
             <div class="w-100"></div>
             @endif
             @if($soquery->SKILL_FSM == "YES")
-            <div class="col-6"style="text-align: left;color: white;"><h2><b>Fire Safety Management</b></h2></div>
+            <div class="col-12"style="text-align: left;color: white;"><h2><b>Fire Safety Management</b></h2></div>
             <div class="w-100"></div>
             @endif
             @if($soquery->SKILL_CERT == "YES")
-            <div class="col-6"style="text-align: left;color: white;"><h2><b>CERT</b></h2></div>
+            <div class="col-12"style="text-align: left;color: white;"><h2><b>CERT</b></h2></div>
             <div class="w-100"></div>
             @endif
             @if($soquery->SKILL_COSEM == "YES")
-            <div class="col-6"style="text-align: left;color: white;"><h2><b>COSEM</b></h2></div>
+            <div class="col-12"style="text-align: left;color: white;"><h2><b>COSEM</b></h2></div>
             <div class="w-100"></div>
             @endif
+            <br>
+            <div class="col-6"style="text-align: left;color: white;"><h4><b>Information Updated As Of<span style="margin-left: 20px;">:</span></b></h4></div>
+            <div class="col-4" style="color: white;"><h4>{{date('d F Y ',strtotime($soquery->CreationDate))}}</h4></div>
+            <div class="w-100"></div>
         </div>
     </div>
 </div>
-{{--End Dekstop--}}
+</div>
+    {{--End Dekstop--}}
 
 {{--Phone--}}
 <div class="visible-xs hidden-md">
@@ -140,7 +148,12 @@
         </div>
     </div>
     <div class="col-12">
+        <div class="row" style="background-color: #C7C7C7; display: flex;
+  justify-content: center; /* horizontal */
+  align-items: center; ">
         <h3><b>TRAINING COMPETENCY</b></h3>
+        </div>
+        <br>
         <div class="row">
             @if($soquery->TR_CCTC == "YES")
                 <div class="col-6"style="text-align: left;"><img src="{{URL::asset('/img/logo/Logo_1.jpg')}}" style="width: 50%;"><span style="margin-left: 5px;"></span></div>
@@ -185,35 +198,47 @@
             @endif
         </div>
     </div>
+{{--    <div class="col-12">--}}
+{{--        <div class="row">--}}
+{{--            <div class="col-6"style="text-align: left"><h6><b>Date Of Updated Courses<span style="margin-left: 20px;">:</span></b></h6></div>--}}
+{{--            <div class="col-6" style="color: white;"><h6>{{date('d F Y ',strtotime($soquery->CreationDate))}}</h6></div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+    <br>
     <div class="col-12">
-        <div class="row">
-            <div class="col-6"style="text-align: left"><h6><b>Date Of Updated Courses<span style="margin-left: 20px;">:</span></b></h6></div>
-            <div class="col-6" style="color: white;"><h6>{{date('d F Y ',strtotime($soquery->CreationDate))}}</h6></div>
+        <div class="row" style="background-color: #C7C7C7; display: flex;
+  justify-content: center; /* horizontal */
+  align-items: center; ">
+                <h3>
+                    <center>
+                    <b>SKILL SETS ACQUIRED</b>
+                    </center>
+                </h3>
         </div>
-    </div>
-    <div class="col-12">
-        <h3><b>SKILL SETS ACQUIRED</b></h3>
         <div class="row">
             @if($soquery->SKILL_BFM = "YES")
-            <div class="col-12"style="text-align: left;color: white;" ><h4><b>Basic Facilities Management</b></h4></div>
+            <div class="col-12"style="text-align: left;color: white;" ><h5><b>Basic Facilities Management</b></h5></div>
             <div class="w-100"></div>
             @endif
             @if($soquery->SKILL_BSS = "YES")
-            <div class="col-12"style="text-align: left;color: white;"><h4><b>Fundamentals of Building Services & Safety</b></h4></div>
+            <div class="col-12"style="text-align: left;color: white;"><h5><b>Fundamentals of Building Services & Safety</b></h5></div>
             <div class="w-100"></div>
             @endif
             @if($soquery->SKILL_FSM = "YES")
-            <div class="col-12"style="text-align: left;color: white;"><h4><b>Fire Safety Management</b></h4></div>
+            <div class="col-12"style="text-align: left;color: white;"><h5><b>Fire Safety Management</b></h5></div>
             <div class="w-100"></div>
             @endif
             @if($soquery->SKILL_CERT = "YES")
-            <div class="col-12"style="text-align: left;color: white;"><h4><b>CERT</b></h4></div>
+            <div class="col-12"style="text-align: left;color: white;"><h5><b>CERT</b></h5></div>
             <div class="w-100"></div>
             @endif
             @if($soquery->SKILL_COSEM = "YES")
-            <div class="col-12"style="text-align: left;color: white;"><h4><b>COSEM</b></h4></div>
+            <div class="col-12"style="text-align: left;color: white;"><h5><b>COSEM</b></h5></div>
             <div class="w-100"></div>
             @endif
+            <div class="col-6"style="color: white;text-align: left"><h6><b>Date Of Updated Courses<span style="margin-left: 20px;">:</span></b></h6></div>
+            <div class="col-6" style="color: white;"><h6>{{date('d F Y ',strtotime($soquery->CreationDate))}}</h6></div>
+            <div class="w-100"></div>
         </div>
     </div>
 </div>
