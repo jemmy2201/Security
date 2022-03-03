047d185cbdf8f3c79f95700be9168237b802bfce
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2022-03-03 09:50:48 +0700

    revisi 1 Followup_Actions_2022_Mar_05

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -91,8 +91,7 @@
                     </div>
                     <div class="col-sm-3"  >
                     </div>
-                    <div class="col-sm-2 hidden-xs" style="position: absolute;
-    margin-left: 1000px">
+                    <div class="col-sm-2 hidden-xs" style="position: absolute;margin-left: 1080px">
                         <h2 style="text-align: center;width:85px;color: white;border: 1px solid white;font-family:neue;">FAQs</h2>
                     </div>
 
