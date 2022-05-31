78c69c09c27d317c1173e8220e2ee67faa141464
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2022-05-31 18:06:39 +0700

    fix bugs 3 paynow receipt

--- resources/views/payment_detail.blade.php
+++ resources/views/payment_detail.blade.php
@@ -900,7 +900,7 @@
             editable: true,             //Whether or not to allow editing of payment amount. Defaults to false if amount is specified
             expiry: {!!  json_encode( date('Ymd', strtotime( date("Ymd"). ' + 14 days')) ) !!},         //Set an expiry date for the Paynow QR code (YYYYMMDD). If omitted, defaults to 5 years from current time.
             {{--            refNumber: {!!  json_encode(refNumber) !!} + " " +{!!  json_encode( $booking_schedule->receiptNo) !!},   //Reference number for Paynow Transaction. Useful if you need to track payments for recouncilation.--}}
-            refNumber: {!!  json_encode($booking_schedule->receiptNo) !!},
+            refNumber: receipt,
             // refNumber: "Website Testing reference number",   //Reference number for Paynow Transaction. Useful if you need to track payments for recouncilation.
             company:{!!  json_encode(refNumber) !!}   //Company name to embed in the QR code. Optional.
         });
