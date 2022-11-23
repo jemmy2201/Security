41fa0511c5fec47b299906e3dd36fef6ea431820
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2022-11-23 12:29:05 +0700

    revisi position singpass

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -166,11 +166,18 @@
                                         {{-- End QR Code --}}
                                         @if(detect_url() == URLUat || detect_url() == LocalHost)
                                             <a href="https://stg-id.singpass.gov.sg/auth?client_id={{clientIdSinpassUat}}&redirect_uri={{redirectUrlSingpassUat}}&scope=openid&response_type=code&state=dummySessionState&nonce=dummySessionState">
-                                                <button type="button" class="btn btn-danger btn-lg">Login SingPass</button>
+                                                <button type="button" class="btn btn-danger btn-lg hidden-xs" style="margin-top: 60px">Login SingPass</button>
                                             </a>
+                                            <a href="https://stg-id.singpass.gov.sg/auth?client_id={{clientIdSinpassUat}}&redirect_uri={{redirectUrlSingpassUat}}&scope=openid&response_type=code&state=dummySessionState&nonce=dummySessionState">
+                                                <button type="button" class="btn btn-danger btn-lg visible-xs hidden-md">Login SingPass</button>
+                                            </a>
+
                                         @else
                                             <a href="https://id.singpass.gov.sg/auth?client_id={{clientIdSinpassProd}}&redirect_uri={{redirectUrlSingpassProd}}&scope=openid&response_type=code&state=dummySessionState&nonce=dummySessionState">
-                                                <button type="button" class="btn btn-danger btn-lg">Login SingPass</button>
+                                                <button type="button" class="btn btn-danger btn-lg hidden-xs" style="margin-top: 60px">Login SingPass</button>
+                                            </a>
+                                            <a href="https://id.singpass.gov.sg/auth?client_id={{clientIdSinpassProd}}&redirect_uri={{redirectUrlSingpassProd}}&scope=openid&response_type=code&state=dummySessionState&nonce=dummySessionState">
+                                                <button type="button" class="btn btn-danger btn-lg visible-xs hidden-md">Login SingPass</button>
                                             </a>
                                         @endif
                                         <center>
@@ -273,11 +280,17 @@
     {{--                                    <div id="ndi-qr"></div>--}}
                                         @if(detect_url() == URLUat || detect_url() == LocalHost)
                                             <a href="https://stg-id.singpass.gov.sg/auth?client_id={{clientIdSinpassUat}}&redirect_uri={{redirectUrlSingpassUat}}&scope=openid&response_type=code&state=dummySessionState&nonce=dummySessionState">
-                                                <button type="button" class="btn btn-danger btn-lg">Login SingPass</button>
+                                                <button type="button" class="btn btn-danger btn-lg hidden-xs" style="margin-top: 60px">Login SingPass</button>
+                                            </a>
+                                            <a href="https://stg-id.singpass.gov.sg/auth?client_id={{clientIdSinpassUat}}&redirect_uri={{redirectUrlSingpassUat}}&scope=openid&response_type=code&state=dummySessionState&nonce=dummySessionState">
+                                                <button type="button" class="btn btn-danger btn-lg visible-xs hidden-md">Login SingPass</button>
                                             </a>
                                         @else
                                             <a href="https://id.singpass.gov.sg/auth?client_id={{clientIdSinpassProd}}&redirect_uri={{redirectUrlSingpassProd}}&scope=openid&response_type=code&state=dummySessionState&nonce=dummySessionState">
-                                                <button type="button" class="btn btn-danger btn-lg">Login SingPass</button>
+                                                <button type="button" class="btn btn-danger btn-lg hidden-xs" style="margin-top: 60px">Login SingPass</button>
+                                            </a>
+                                            <a href="https://id.singpass.gov.sg/auth?client_id={{clientIdSinpassProd}}&redirect_uri={{redirectUrlSingpassProd}}&scope=openid&response_type=code&state=dummySessionState&nonce=dummySessionState">
+                                                <button type="button" class="btn btn-danger btn-lg visible-xs hidden-md">Login SingPass</button>
                                             </a>
                                         @endif
                                         <center>
