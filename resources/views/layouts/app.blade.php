682c674c3abef0aca1d367cc157e7206a637cb25
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2022-12-29 14:15:59 +0700

    dissable inspect

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -196,7 +196,25 @@
     </div>
     <script type="application/javascript">
         $( document ).ready(function() {
+            // Disable inspect
+            document.addEventListener('contextmenu', (e) => e.preventDefault());
 
+            function ctrlShiftKey(e, keyCode) {
+                return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+            }
+
+            document.onkeydown = (e) => {
+                // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+                if (
+                    event.keyCode === 123 ||
+                    ctrlShiftKey(e, 'I') ||
+                    ctrlShiftKey(e, 'J') ||
+                    ctrlShiftKey(e, 'C') ||
+                    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+                )
+                    return false;
+            };
+            // End Disable inpect
             // var TextAreaName = document.getElementById('TextAreaName');
             // TextAreaName.value = TextAreaName.value.replace(/^\s*|\s*$/g,'');
             //
