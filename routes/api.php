110b230ef1143bdf0051f54e7565244c9924b7b9
jemmy <cchaliansyah@gmail.com>
2023-07-13 09:33:23 +0700

    cek change data passid to receiptNo

--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,7 @@ Route::post('/secret_decode',function(Request $request){
 });
 
 Route::post('/check/passid',function(Request $request){
-    $data = booking_schedule::where('passid','LIKE','%'.$request->passid.'%')->get();
+//    $data = booking_schedule::where('receiptNo','LIKE','%'.$request->receiptNo.'%')->get();
+    $data = booking_schedule::where(['receiptNo'=>$request->receiptNo])->get();
     return $data;
 });
