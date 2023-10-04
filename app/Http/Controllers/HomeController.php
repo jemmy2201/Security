aafe89cefc1afdc634be728689e03266e116eb59
darto+1-at-378186349712 <jemmyrolish07@gmail.com>
2023-10-04 14:00:09 +0700

    add filter card issue N show page error

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1120,7 +1120,7 @@ class HomeController extends Controller
 ////                                return view('page_error')->with(['data1' => value_not_found1, 'data2' => value_not_found2, 'image' => 'fa fa-info-circle']);
 //                    return view('page_error')->with(['data1' => value_not_found1, 'data2' => value_not_found3, 'data3' => value_not_found4, 'image' => 'fa fa-info-circle']);
 //                }
-                if (Carbon::today()->toDateString() >= $less_expired_date && $f->card_issue != NotExpiredCard ) {
+                if (Carbon::today()->toDateString() >= $less_expired_date && $f->card_issue != RemoveExpiredCard || $f->card_issue == NotExpiredCard ) {
                     return view('page_error')->with(['data1' => expired_less_3month, 'data4' => value_not_found2, 'data3' => value_not_found6, 'data2' => value_not_found5, 'image' => 'fa fa-info-circle']);
                 }
             }
